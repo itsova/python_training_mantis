31029356e637a9ae0dbcb3b5c680276d70ed3d75
zakur <anastasiyavolkova165@gmail.com>
2020-05-19 18:27:24 +0300

    проверки списков через soap fix

--- fixture/soap.py
+++ fixture/soap.py
@@ -7,7 +7,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -15,13 +15,14 @@ class SoapHelper:
             return False
 
     def project_lists(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             list = client.service.mc_projects_get_user_accessible(username, password)
             return list
         except WebFault:
             list = []
             return list
+        
 
 
 
